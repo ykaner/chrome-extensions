fecf023bd10c50fca4d60fe795953b8e8e9d01aa
ykaner <ykaner12@gmail.com>
2017-06-02 16:31:46 +0300

    Update google fagots.js

--- google fagots.js
+++ google fagots.js
@@ -6,7 +6,7 @@ function killGoogleFagots(){
 	newlogo.innerHTML = '<img style="padding-top:112px" height="92" src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width="272" alt="Google" id="hplogo" title="Google" onload="google.aft&amp;&amp;google.aft(this)">';
 	newlogo = newlogo.content.firstChild;
 
-	logo.parentNode.id = 'doodle';
+	logo.parentNode.onckick = undefined;
 	logo.parentNode.appendChild(newlogo);
 	logo.parentNode.removeChild(logo);
 
