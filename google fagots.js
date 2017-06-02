69aafd9feeaf1d88a43538d2ad8e9a7239cc0017
ykaner <ykaner12@gmail.com>
2017-06-02 15:16:28 +0300

    Add files via upload

---
+++ google fagots.js
@@ -0,0 +1,15 @@
+function killGoogleFagots(){
+
+	var logo = document.getElementById('hplogo');
+
+	var newlogo = document.createElement('template');
+	newlogo.innerHTML = '<img style="padding-top:112px" height="92" src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width="272" alt="Google" id="hplogo" title="Google" onload="google.aft&amp;&amp;google.aft(this)">';
+	newlogo = newlogo.content.firstChild;
+
+	logo.parentNode.id = 'doodle';
+	logo.parentNode.appendChild(newlogo);
+	logo.parentNode.removeChild(logo);
+
+}
+
+setTimeout(killGoogleFagots, 300);
